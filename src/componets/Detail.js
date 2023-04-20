d7e998f5cdacd91952ba3dbce2ae24a042af377a
Ismail K <ismailkoormath037@gmail.com>
2023-04-20 10:18:04 +0530

    remove some files

--- src/componets/Detail.js
+++ src/componets/Detail.js
@@ -7,7 +7,6 @@ const Detail = () => {
     <div style={{backgroundColor:"white" ,margin:"30px",padding:"20px"}} >
         <div className='detail-top'>
         <img  style={{width:"300px" ,marginRight:"100px",marginBottom:"50px"}} src='https://thumbs.dreamstime.com/b/crispy-fried-chicken-wings-wooden-table-kentucky-88919528.jpg'></img>
-        
         <div>
             <h3>title</h3>
             <p>discription </p>
